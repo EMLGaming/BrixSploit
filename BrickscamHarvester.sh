0ce61733c662fc9475677cef37ef6a42051dfcbe
Sefa Eyeoglu <contact@scrumplex.net>
2018-07-07 16:01:34 +0200

    Improve wording in harvester script

--- BrickscamHarvester.sh
+++ BrickscamHarvester.sh
@@ -1,19 +1,21 @@
-#! /bin/bash
+#!/bin/bash
+set -e
 
-printf "\nEnter your Shodan API key: \n"
+echo "Enter your Shodan API key:"
 read key
 shodan init $key
 
-printf "\nOutput file name: \n"
+echo "Enter output file name:"
 read outputname
-printf "\nAmount of cameras \n"
+
+echo "Enter amount of cameras (free accounts up to 20):"
 read amount
+
+echo "Downloading list of Brickcom devices..."
 shodan download --limit $amount temp.json.gz Brickcom
 
+echo "Parsing downloaded list..."
 shodan parse --fields ip_str,port --separator : temp.json.gz | rev | cut -c 2- | rev >> $outputname>&1
-printf "\nConverting temp.json.gz to $outputname"
-printf "\nRemoving temp.json.gz"
 rm temp.json.gz
-printf "\nDONE! Your list is in $outputname\n"
 
-exit
+echo "\nDONE! Your list is in $outputname\n"
