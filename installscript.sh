305ae4812c09e741c11205b914be567ebc7ced03
Sefa Eyeoglu <contact@scrumplex.net>
2018-07-07 15:59:41 +0200

    Use pip instead of easy_install

--- installscript.sh
+++ installscript.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install python-setuptools
+sudo apt-get install python3-pip
 sudo apt-get install yad
-sudo easy_install shodan
+sudo pip3 install shodan
 sudo chmod +x *sh
